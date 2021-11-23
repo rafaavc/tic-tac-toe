389872b9cde6fc027be0815d4f09a4116ab01c6a
rafaavc <rafavc2508@gmail.com>
2021-11-23 15:15:28 +0000

    Reduced default mechine time to think to 2s

--- project/src/jsMain/kotlin/controller/GameSettings.kt
+++ project/src/jsMain/kotlin/controller/GameSettings.kt
@@ -8,7 +8,7 @@ data class GameSettings(
     var player1Piece: GamePiece = GamePiece.O,
     var boardSize: Int = 10,
     var target: Int = 5,
-    var timeToThink: Double = 3.0,
+    var timeToThink: Double = 2.0,
     var moveStrategyFactory: MoveStrategyFactory = HvMStrategyFactory()
 ) {
     fun set(player1Piece: GamePiece, boardSize: Int, target: Int, timeToThink: Double, moveStrategyFactory: MoveStrategyFactory) {
